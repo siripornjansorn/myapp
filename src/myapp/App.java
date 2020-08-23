9891dd3e4b0efc2cbb3767b0fc46402a61ded67b
siriporn jansorn <61050891@kmitl.ac.th>
2020-08-23 21:51:44 +0700

    Merge branch 'feature2' and resolve conflict

--- src/myapp/App.java
+++ src/myapp/App.java
@@ -2,10 +2,8 @@ package myapp;
 
 public class App {
     public static void main(String[] args) {
-<<<<<<< HEAD
+
         System.out.println("Hello Master!");
-=======
-        System.out.println("Hello Feature2!");
->>>>>>> feature2
+
     }
 }
